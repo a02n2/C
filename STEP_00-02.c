92a032a68f1c80ab32a356b62d46c120a0252c30
a02n2 <49223782+a02n2@users.noreply.github.com>
2019-04-16 13:41:44 +0900

    STEP_00-02

---
+++ STEP_00-02.c
@@ -0,0 +1,26 @@
+int main(void) {
+
+	int i;
+	unsigned int ui;
+	short h;
+	unsigned short uh;
+	unsigned int ui16;
+
+	i = -2147483648;
+	i = 2147483647;
+
+	ui = 0;
+	ui = 4294967295;
+
+	h = -32768;
+	h = 32767;
+
+	uh = 0;
+	uh = 65535;
+
+	ui16 = 0;
+	ui16 = 0xFFFFFFFF;
+
+	return 0;
+
+}
