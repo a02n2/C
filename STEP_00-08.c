d0038d0dd13d7747f79e6733161a641b5e271f4a
a02n2 <49223782+a02n2@users.noreply.github.com>
2019-04-17 16:39:33 +0900

    Update STEP_00-08.c

--- STEP_00-08.c
+++ STEP_00-08.c
@@ -1,20 +1,26 @@
-void func1(void) {
-	return;
-}
+void func1(void);
+int func2(void);
+char *func3(void);
 
-/*戻り値２５５*/
-int func2(void) {
-	return 255;
-}
-
-/*戻り値あいうえお*/
-char *func3(void){
-	return "あいうえお";
-}
-
-/*funcを呼び出し*/
 int main(void) {
 	func1();
 	func2();
 	func3();
+	
+	return 0;
+}
+
+/*func1呼び出し*/
+void func1(void) {
+	return;
+}
+
+/*func2呼び出し*/
+int func2(void) {
+	return 255;
+}
+
+/*func3呼び出し*/
+char *func3(void){
+	return "あいうえお";
 }
