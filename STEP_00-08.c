8bb26c500044403c8d8badaab517b9e22c1eb4a7
a02n2 <49223782+a02n2@users.noreply.github.com>
2019-04-17 13:07:36 +0900

    STEP_00-08

--- STEP_00-08.c
+++ STEP_00-08.c
@@ -2,14 +2,17 @@ void func1(void) {
 	return;
 }
 
+/*–ß‚è’l‚Q‚T‚T*/
 int func2(void) {
 	return 255;
 }
 
+/*–ß‚è’l‚ ‚¢‚¤‚¦‚¨*/
 char *func3(void){
 	return "‚ ‚¢‚¤‚¦‚¨";
 }
 
+/*func‚ğŒÄ‚Ño‚µ*/
 int main(void) {
 	func1();
 	func2();
